17c73a5bfbe0da9cee49e2c412e04a4fa0025c93
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-06 22:25:13 +0100

    add help info to powerIteration

--- powerIteration.m
+++ powerIteration.m
@@ -1,3 +1,24 @@
+% [eigenvalue, x] = powerIteration(A, x0, delta, iterationsLimit) - funkcja
+% wykorzystujaca metode potegowa z normowaniem do obliczenia dominujacej
+% wartosci wlasnej eigenvalue macierzy A oraz odpowiadajacego jej wektora
+% wlasnego x, zaczynajac z przyblizeniem poczatkowym wektora wlasnego x0.
+%
+% Funkcja wykonuje obliczenia az limit iteracji iterationsLimit zostanie
+% przekroczony, albo modul roznicy miedzy kolejnymi przyblizeniami wartosci
+% wlasnych bedzie mniejszy niz delta.
+%
+% Wejscie:
+% * A - macierz dla ktorej wartosci wlasnych szukamy
+% * x0 - przyblizenie poczatkowe wektora wlasnego
+% * delta - zadana dokladnosc (domyslnie 0.01)
+% * iterationsLimit - ograniczenie iteracji (domyslnie 100)
+%
+% Wyjscie:
+% * eigenvalue - dominujaca wartosc wlasna macierzy A
+% * x - wektor wlasny odpowiadajacy wartosci wlasnej eigenvalue
+%
+% Autor: Grzegorz Rozdzialik (D4, gr. lab. 2)
+
 function [eigenvalue, x] = powerIteration(A, x0, delta, iterationsLimit)
 if nargin < 4
     iterationsLimit = 100;
@@ -19,7 +40,7 @@ for k=1:iterationsLimit
     eigenvalue = dot(y, x);
     
     % Normowanie
-    x = y / norm(y);
+    x = y ./ norm(y);
     
     % Sprawdzenie warunku stopu
     if (abs(eigenvalue - previousEigenvalue) < delta)
