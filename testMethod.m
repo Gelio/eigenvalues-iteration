171aa40127a449e6230f021d1e87c2e6147e47bd
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-12-10 22:10:15 +0100

    test script: improve stats logging, list execution time, compare results with eig from matlab

--- testMethod.m
+++ testMethod.m
@@ -5,6 +5,9 @@
 % a(k, k-1) = 2 + i
 % a(k, k+1) = 2 - i
 %
+% Skrypt poza przyblizeniem szukanych wartosci i wektorow wlasnych
+% porownuje te wyniki oraz szybkosc obliczen z funkcja eig dostepna w
+% Matlabie.
 %
 % Autor: Grzegorz Rozdzialik (D4, gr. lab. 2)
 
@@ -12,7 +15,7 @@
 % Delta - warunek stopu na maksymalna roznice miedzy kolejnymi
 % przyblizeniami wartosci wlasnej, jezeli bedzie mniejsza to obliczenia nie
 % sa kontynuowane
-delta = 0.001;
+delta = 0.1;
 % Limit iteracji - ograniczenie na liczbe iteracji dla przyblizania
 % pojedynczej wartosci wlasnej
 limitIteracji = 100;
@@ -27,23 +30,48 @@ n = 10;
 A = constructMatrix(n);
 
 % Znalezienie wartosci i wektorow wlasnych
+tic;
 [eigenvalues, eigenvectors] = findEigenvaluesAndVectors(A, delta, limitIteracji);
+methodTime = toc;
 
-% Weryfikacja czy wlasnosci wartosci wlasnych i wektorow wlasnych sa
-% spelnione
+% Informacje dotyczace bledu przyblizenia metoda potegowa
 E = calculateErrorMatrix(A, eigenvalues, eigenvectors);
 Enorm = norm(E);
 
+fprintf('Metoda potegowa z normowaniem:\n');
 fprintf('Maksymalny odchyl A * x - lambda * x: %e\n', max(max(E)));
-fprintf('Norma wektora bledu: %e\n', Enorm);
+fprintf('Norma wektora bledu: %e\n\n', Enorm);
+
 
 % Obliczenie wartosci wlasnych za pomoca funkcji eig dostepnej w Matlabie
-matlabEigenvalues = eig(A);
+tic;
+[matlabEigenvectors, matlabEigenvalues] = eig(A);
+matlabTime = toc;
+matlabEigenvalues = diag(matlabEigenvalues);
 matlabEigenvalues = matlabEigenvalues(end:-1:1);
 
-realEigenvalues = real(eigenvalues);
-fprintf('Znalezione wartosci wlasne (pierwsza kolumna) oraz te obliczone przez Matlaba (funkcja eig, druga kolumna):\n');
-display([realEigenvalues, matlabEigenvalues]);
+% Informacje dotyczace bledu przyblizenia funkcja eig
+EMatlab = calculateErrorMatrix(A, matlabEigenvalues, matlabEigenvectors);
+EMatlabNorm = norm(EMatlab);
 
-fprintf('Norma wektora roznicy wartosci przyblizonych ta metoda oraz obliczonych przez Matlaba: %e\n', ...
-    norm(eigenvalues - matlabEigenvalues));
+fprintf('Funkcja eig dostepna w Matlabie:\n');
+fprintf('Maksymalny odchyl A * x - lambda * x: %e\n', max(max(EMatlab)));
+fprintf('Norma wektora bledu: %e\n\n', EMatlabNorm);
+
+
+
+if n <= 10
+    realEigenvalues = real(eigenvalues);
+    fprintf('Znalezione wartosci wlasne (pierwsza kolumna) oraz te obliczone przez Matlaba (funkcja eig, druga kolumna):\n');
+    display([realEigenvalues, matlabEigenvalues]);
+else
+    fprintf('Znalezione wartosci wlasne znajduja sie w zmiennej "eigenvalues"\n');
+end
+fprintf('\n');
+
+% Statystyki dotyczace czasu przyblizania
+fprintf('Czas obliczania wartosci wlasnych:\n');
+fprintf('* metoda potegowa: %.4f ms\n', methodTime * 1000);
+fprintf('*     funkcja eig: %.4f ms\n', matlabTime * 1000);
+
+fprintf('\n');
